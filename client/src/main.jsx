2dd38921cf3ceea7f3c25bb36b7f08b2c0b31521
Krishika S Jain <krishikajain28@gmail.com>
2026-01-14 20:09:17 +0530

    added vercel analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async"; // IMPORT THIS
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css";
+import { HelmetProvider } from "react-helmet-async";
+import { Analytics } from "@vercel/analytics/react";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <HelmetProvider>
-      {" "}
-      {/* WRAP APP HERE */}
       <App />
+      <Analytics />
     </HelmetProvider>
-  </StrictMode>
+  </React.StrictMode>
 );
